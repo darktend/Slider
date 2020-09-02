43fb26cc43145fdbaadda4e0041dc262c6c16cb6
darktend <vladgodunqwer@gmail.com>
2020-09-02 22:48:49 +0300

    push file of slider

---
+++ js/index.js
@@ -0,0 +1,39 @@
+let btnNext = document.querySelector(' .slider  .buttons .next ');
+let btnPrev = document.querySelector(' .slider  .buttons .prev');
+var images = document.querySelectorAll(' .slider .slider__img img');
+var i = 0;
+
+
+btnNext.onclick = function() {
+    images[i].style.display = 'none';
+    i = i + 1;
+    if (i >= images.length) {
+
+        i = 0;
+
+    }
+    images[i].style = 'block';
+
+};
+
+btnPrev.onclick = function() {
+    images[i].style.display = 'none';
+    i = i - 1;
+    if (i < 0) {
+
+        i = images.length - 1;
+    }
+    images[i].style = 'block';
+
+};
+
+
+
+
+
+
+// автоматическая прокрутка слайдера
+//вместе кнопок кружки слайдера
+/*
+    адаптивный слайдер
+*/
